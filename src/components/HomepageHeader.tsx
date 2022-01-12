7201e25a814f4b461d58dd623623df4708b44df2
Doug <doug@hifi.finance>
2022-01-12 12:11:37 -0700

    Update bug bounty scope and style

--- src/components/HomepageHeader.tsx
+++ src/components/HomepageHeader.tsx
@@ -11,10 +11,10 @@ export default function HomepageHeader() {
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">The fixed-rate, fixed-term lending protocol</p>
+        <p className="hero__subtitle">The Hifi Lending Protocol allows anyone to borrow against their crypto. Hifi uses a bond-like instrument, representing an on-chain obligation that settles on a specific future date. Buying and selling the tokenized debt enables fixed-rate lending and borrowing — something much needed in decentralized finance today.</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/protocol/introduction">
-            Get Started
+          <Link className="button button--primary button--lg" to="/protocol/introduction">
+            Learn More
           </Link>
         </div>
       </div>
